359a4e740aeed2e0b0a83fae31f11af723598776
Neil <neil.stewart@warwick.ac.uk>
2019-01-11 18:32:26 +0000

    Neil trying to run...

--- MDB10.R
+++ MDB10.R
@@ -4,114 +4,112 @@ library(stringi)
 library(lubridate)
 #setwd("C:/Anna/MDB_alcohol")
 load("MDB10.RData")
-#load("MDB10random.RData")
 
-# MDB10_all <- MDB10
-# #select random sample
-# MDB10 <- MDB10_all[sample.int(nrow(MDB10_all),1000000),]
-# save(MDB10, file = "MDB10random.RData")
+#MDB10 <- data.table(MDB10[, c("Transaction.Date", "Transaction.Description")])
 
-#get pubs dataset
-allpubs <- data.table(read.csv("allpubs.csv", header = FALSE))
-#convert to lowercase
-allpubs[, V2 := tolower(V2)]
-#frequency of each name
-allpubs <- allpubs[,.N, .(V2)]
-#keep the ones that 
-allpubs <- allpubs[N>1,]
-allpubs[, twowords := grepl(" ", V2)]
-#keep the two words ones, and filter the one word ones - delete those that are very common words 
-#and can be street names or other businesses etc
-todelete <- c("angel", "swan", "railway", "trinity", "alexandra", "victoria", "castle", "ship",
-              "albion", "silk", "broadway", "red", "fuel", "nine", "windsor", "home", "olivers",
-              "grove", "trafalgar", "boutique", "pure", "curve", "waterloo")
-allpubs[twowords == FALSE, keep := !grepl(paste(todelete, collapse = "|"), V2)]
-
-#unique pub names, plus add "pub", "inn", "tavern", "tap", "malt", "brewery", "arms", 
-pubs <- c(allpubs[twowords == TRUE,V2], allpubs[keep == TRUE,V2],
-          "pub", "inn", "tavern", "tap", "malt", "brewery", "arms")
-#remove characters that will give us trouble
-pubs <- gsub("\\(","", pubs)
-pubs <- gsub("\\)","", pubs)
-pubs <- gsub("\\[","", pubs)
-pubs <- gsub("\\]","", pubs)
-pubs <- gsub("\\\\","", pubs)
-#add space before and after
-#pubs <- paste0("\\<",pubs,"\\>")
-
-
-
-# I have 52k pub names and  1 million unique transactions
-# so try to filter out very common entries that are dre defo not alcohol related to reduce computation time
-#  words <- unique_transactions_all$description
-# # #get rid of numbers
-#  words <- gsub("1|2|3|4|5|6|7|8|9|0|,|-", "", words)
-#  words <- unique(words)
-#  frequency <- data.table(table(unlist(strsplit(words, split = " "))))
-#  frequency <- frequency[order(-N)]
-# based on the inspection of the first 500, we can use these words to filter out
-# non alcohol related purchases
+#remove those with no info
+MDB10 <- MDB10[!(Transaction.Description %in% c("<MDBREMOVED>", "<mdbremoved>", "<mdbremoved> - chq")),]
+MDB10[, Transaction.Description := tolower(Transaction.Description)]
 
 nonalcohol <- c("tesco", "stores", "group", "uber", "withdrawal", "sainsb",
                 "asda", "coop", "lidl", "aldi", "waitrose", "spencer", "spar",
-                "petrol", "shell", "esso", "morrisons", "hotel", "travel",
+                "petrol", "shell", "esso", "morrisons", "hotel", "travel","sainsburys",
                 "google", "restaurants", "pizza", "starbucks", "next", "superstore",
                 "garden", "nandos", "subway", "primark", "greggs", "argos", 
                 "superdrug", "costa", "amazon", "internet", "coffee", "cafe","caffe",
                 "donalds", "spotify", "smith", "manger", "kfc", "ticket","mcdonalds",
-                "boots", "domestic", "food", "grocery","h&m","pharmacy",
+                "boots", "domestic", "food", "grocery","h&m","pharmacy","costco",
                 "ikea", "poundland", "cineworld", "lewis", "dental", "debenhams",
-                "parking", "wilko", "halfords", "zara", "iceland", "homebase",
-                "wagamama", " atm ", " fee ", "int'l", "mortgage", "weightwatchers",
+                "parking", "wilko", "halfords", "zara", "iceland", "homebase","giffgaff",
+                "wagamama", "atm", "fee", "int'l", "mortgage", "weightwatchers",
                 "energy", "standing order", "national trust", "steak", "burger king",
-                "post office", "lloyds", "virgin active", "fitness", "co-op",
-                "theatre", "park view nursery", "farm park", "health", "hamburgers",
+                "post office", "lloyds", "virgin active", "fitness", "co-op","virgin media",
+                "theatre", "park view nursery", "farm park", "health", "hamburgers","virgin mobile",
                 "cinemas", "itunes","amazon.co.uk","itunes.com", "hotel","marketplace",
-                "sainsbury's",  "marks&spencer", "lewis", "interest", "morrison",
-                "water", "mandate", "halifax", "hsbc", "insurance","tfl.gov.uk/cp",
-                "www.just", "lottery", "connect", "paypal", "council", "pets",
-                "www.skybet.com", "vodafone", "barclays", "barclaycard", "tv",
-                "currys", "bbc", "loan", "restaurant", "netflix.com", "trains",
-                "rail","bargains","oyster", "barclay", "refund", "betfair",
-                "warehouse","ocado")
-#only match entire words
-nonalcohol <- paste0("\\b", nonalcohol, "\\b")
-pubs <-  paste0("\\b", pubs, "\\b")
+                "sainsbury's",  "marks&spencer", "lewis", "interest", "morrison","nespresso",
+                "water", "mandate", "halifax", "hsbc", "insurance","tfl.gov.uk/cp","hotels",
+                "www.just", "lottery", "connect", "paypal", "council", "pets", "tea",
+                "www.skybet.com", "vodafone", "barclays", "barclaycard", "tv","newsagent",
+                "currys", "bbc", "loan", "restaurant", "netflix.com", "trains","cleaners","cleaner",
+                "rail","bargains","oyster", "barclay", "refund", "betfair","eurogarages",
+                "warehouse","ocado", "groupon", "chq", "juice", "cashback","hair","hairdressin",
+                "holland & barrett", "weight", "holiday inn", "premier inn", "jurys inn",
+                "services","service", "takeaway", "foundation", "garage", "flowers", "news",
+                "nursery", "shop", "florist", "irish int", "vets", "servic", "souvenir", "car",
+                "motors", "crafts", "white rose", "books", "hatch", "mult", "sushi",
+                "vetr", "county oak", "park", "farm", "market", "centre", "texaco", "frankie & bennys",
+                "store", "murco", "selly oak", "supermarket", "furniture", "cheshire oak", "fish",
+                "sandwich", "bakery", "bullring", "bull ring", "bull ring-bir", "motor", "cinema",
+                "bengal", "thai", "pull and bear", "clinic", "gallery", "globe london", "ferries",
+                "sheilas'", "tyre", "hall", "convenience", "climbing", "chinese", "indian", "collectors",
+                "supervalu", "fish", "newsagents", "bells brook", "shoes", "bow bells", "charity",
+                "boutique", "movie", "t-mobile", "thank", "specsavers", "save", "usage", "faster",
+                "sky", "bill", "life", "tax", "express", "capital", "transfer", "charge", "b365",
+                "automated", "retail", "limited", "o2", "bac", "british")
 
-####################NON-ALCOHOL FLAG###########################
-#create non-alcohol flag, we can exclude these when matching the pub names
-#MDB10[, nonalc := stri_detect_regex(Transaction.Description, paste(nonalcohol, collapse = "|"))]
-#150 seconds with 1 million
+
+#Using regular expressions would be better, but it takes more than twice as long as using stri_detect_fixed
+nonalcohol <- paste0("\\b", nonalcohol, "\\b")
+write(nonalcohol, file="noalcohol_keywords.csv")
 
 MDB10[, nonalc := 0]
-  system.time(
-    for (i in 1:length(nonalcohol)) {
+  for (i in 1:length(nonalcohol)) {
     MDB10[nonalc == 0, nonalc := nonalc + as.numeric(stri_detect_regex(Transaction.Description, nonalcohol[i]))]
-    #print(i)
-  })
-#55 seconds with 1 million - 3 times faster
+    print(i)
+  }
 
+#for a 10% sample (26 million), this takes about 40 minutes
 
-####################ALCOHOL FLAG###########################
+toadd <- c("inn","arms","arm","bar","bars","tavern","tav","brewhouse","conservative","olde","public house",
+           "sports","pub","pubs","social club", "ale", "ales", "tap", "taphouse", "malt", "malthouse","beer",
+           "brew", "stag","hound" ,"head", "and crown", "& crown", "crown &", "crown and", "the crown","beerhouse",
+           "famous crown", "old crown", "lion", "revolution", "smiths of", "the red", "duke on the green",
+           "fox on the green", "the plough", "plough and", "plough &", "windmill", "be at one", "the queen",
+           "the queens", "the cock", "and cock", "& cock", "cock &", "cock and", "the black", "oneills",
+           "o'neills", "the king", "the kings", "king and", "wheatsheaf", "the george", "admiral nelson",
+           "lord nelson", "captain nelson", "the nelson", "walkabout", "rose", "pitcher & piano", "cosy club",
+           "the fox", "fox &", "fox and", "and fox", "& fox", "hare", "hen", "raven", "alehouse", "thatch",
+           "barrel", "thistle", "oak", "greyhound", "chequers", "alchemist", "botanist", "prince of",
+           "the victoria", "the ox", "beehive", "golden bee", "honey bee", "the hop", "goose", "swan",
+           "the star", "star and", "star &", "brewers", "and castle", "& castle", "the castle", "the wharf",
+           "cross keys", "the cross", "the duke", "the railway", "the mill", "dog and", "horse",
+           "the albion", "robin hood", "anchor", "the boat", "boathouse", "the duck", "the ship", "ship and",
+           "the kingfisher", "half moon", "free house", "the old", "the bear", "and bear", "smokehouse",
+           "smoke house", "the cat", "the shakespeare", "the globe", "crowns", "the cow", "boozy cow",
+           "spotted cow", "dun cow", "calf", "wig", "coach", "tree", "the union","trees",
+           "earl of", "wheel", "the dolphin", "wellington", "bridge house", "the folly", "marquis",
+           "the yard", "royal albert", "smugglers", "queen of", "the engine", "the full moon", "thieves",
+           "the shack", "the bank", "the regent", "the temple", "the talbot", "the admiral", "old crown",
+           "corner house", "punch bowl", "fire station", "the pit", "the mayflower", "the quay", "queen victoria",
+           "the courtyard", "the cambridge", "the dragon", "the hawk", "bunch of grapes", "the snug",
+           "sailor", "the social", "waxy o'connors", "waxy oconnors", "harp", "platform","roof","grapes",
+           "prospect of whitby", "pheasant", "horses", "blue boar", "squirrel", "smoking goat","the phoenix",
+           "thirsty", "the goat", "goat &", "queen adelaide", "the fable", "the pig", "bells", "the fleece",
+           "the hope", "foresters", "packhorse","the lighthouse", "hand", "ivy house", "the vault",
+           "the navigation", "greene king", "wetherspoon", "wetherspoons", "yates", "yates's", "yatess",
+           "hobgoblin", "slug and lettuce", "stonegate", "queen elizabeth", "royal george","stables",
+           "seven kings", "hole in the", "angelic", "priory", "dukes", "anthologist", "marquess","taver",
+           "cocks", "bull", "cricketers", "distiller", "duchess", "prince albert", "brasserie", "regent",
+           "the queens", "the flag", "brewer", "aragon", "woolpack", "rise 46", "the bat", "the mulberry",
+           "flying pig", "treehouse", "old mill", "draft house", "hammer & pincers", "minnow", "the sun",
+           "wine", "coopers", "under the", "marston", "drinks", "old manor", "spotted", "port royal",
+           "yeates", "mudlark", "of kings", "maypole", "willow walk", "cellar", "waggon", "token house")
+compound <- c(paste("red", c("light","rover", "deer", "cow", "fox")),
+              paste("white", c("hart", "heart", "swan", "rabbit", "cross", "rose", "bear", "house", "lion")),
+              paste("the green", c("goose", "dragon", "man")), 
+              paste("the royal", c("oak", "exchange", "william", "george", "scot", "standard", "pug", "sun", "barn")),
+              paste("black", c("swan", "cock", "friar", "bear", "lion", "boy","heart", "fox", "bear", "dog")), 
+              paste(c("lazy","running", "snooty", "hyndland", "stretton"),"fox"))
 
+alcohol <- c(paste0("\\b", c(toadd[!grepl("&", toadd)], compound), "\\b"), toadd[grepl("&", toadd)])
+#there's something weird going on with "&" so I'm gonna remove the \\b from these otherwise it doesnt match them
 
-#create alcohol flag for subset of data
-# system.time(MDB10[nonalc == 0, 
-#                    alco := stri_detect_regex(Transaction.Description,paste(pubs, collapse = "|"))])
-#  MDB10[is.na(alco), alco := 1]
-#282 seconds with 100k entries
-#47 minutes with 1 million
+write(alcohol, file="alcohol_keywords.csv")
 
 MDB10[, alc := 0]
-system.time(
-for (i in 1:length(pubs)) {
-  MDB10[nonalc == 0 & alc == 0, alc := alc + as.numeric(stri_detect_regex(Transaction.Description, pubs[i]))]
-print(i)
-})
+  for (i in 1:length(alcohol)) {
+    MDB10[nonalc == 0 & alc == 0,  alc := alc + as.numeric(stri_detect_regex(Transaction.Description, alcohol[i]))]
+    print(i)
+  }
 
-#119 seconds with 100k entries
-#1232.05 (21 minutes) seconds with 1 million entries
-#demonstrate that it is the same result
-#all.equal(as.numeric(MDB10$alco), MDB10$alc)
-
-save(MDB10, file="MDB10_alc.RData")
+#this takes about 40 mins too
